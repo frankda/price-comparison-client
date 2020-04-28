8aa7194d88bed1374feed9d689cf16af3012ca09
frankda <suyunyi@gmail.com>
2020-04-28 22:28:24 +1000

    update server url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const SERVER_URL = 'https://price-before-you-go.herokuapp.com/search';
+const TEST_SERVER_URL = 'http://localhost:3001/search'
+
 const Scrape = {
   searchProduct(productname) {
-    return axios.post(`http://localhost:3001/search`, { productname: productname });  // send an object with key named productname
+    return axios.post(TEST_SERVER_URL, { productname: productname });  // send an object with key named productname
   }
 }
 
