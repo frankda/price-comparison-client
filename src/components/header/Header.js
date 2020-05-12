309b39d9a907607fa3bf4cc1480e46cf1db5df73
frankda <36907108+frankda@users.noreply.github.com>
2020-05-12 18:32:11 +1000

    modify header structural

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import DivideLine from '../svg/divide';
+import './header.scss';
 
 export default () => {
   return (
     <header className="header">
-      <div className="logo">
+      <div className="header__logo">
         <img src="" alt="" />
         <h1>Price Comparison</h1>
       </div>
-      <div className="header__user">
+      {/* <div className="header__user">
         <p>Sign up</p>
         <DivideLine height={12} />
-      </div>
+      </div> */}
     </header>
   );
 }
